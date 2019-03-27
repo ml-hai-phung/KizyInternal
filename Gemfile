fca4c1cc9400ef7d19873dafd1a81396cb9c58e3
Hai Phung N.T <hai.phung@asiantech.vn>
2019-03-27 10:58:00 +0700

    Use boiler template

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+
+ruby '2.4.2'
+
+gem 'xcpretty', '0.3.0'
+gem 'cocoapods', '1.5.3'
+gem 'linterbot', '0.2.7'
+gem 'fastlane', '2.118.1'
---
+++ MyApp/Model/API/Core/RequestSerializer.swift
@@ -0,0 +1,45 @@
+//
+//  RequestSerializer.swift
+//  KizyApp
+//
+//  Created by iOSTeam on 2/21/18.
+//  Copyright © 2016 AsianTech Co., Ltd. All rights reserved.
+//
+
+import Alamofire
+import Foundation
+
+extension ApiManager {
+    @discardableResult
+    func request(method: HTTPMethod,
+                 urlString: URLStringConvertible,
+                 parameters: [String: Any]? = nil,
+                 headers: [String: String]? = nil,
+                 completion: Completion?) -> Request? {
+        guard Network.shared.isReachable else {
+            completion?(.failure(Api.Error.network))
+            return nil
+        }
+
+        let encoding: ParameterEncoding
+        if method == .post {
+            encoding = JSONEncoding.default
+        } else {
+            encoding = URLEncoding.default
+        }
+
+        var aHeaders = api.defaultHTTPHeaders
+        aHeaders.updateValues(headers)
+
+        let request = Alamofire.request(urlString.urlString,
+                                        method: method,
+                                        parameters: parameters,
+                                        encoding: encoding,
+                                        headers: aHeaders
+        ).responseJSON(completion: { (response) in
+            completion?(response.result)
+        })
+
+        return request
+    }
+}
