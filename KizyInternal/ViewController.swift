d736c78cef053ae12b75236863b65d15f0371189
Hai Phung N.T <hai.phung@asiantech.vn>
2019-03-27 09:39:30 +0700

    Init project

---
+++ KizyInternal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KizyInternal
+//
+//  Created by Hai Phung N.T. on 3/26/19.
+//  Copyright © 2019 Hai Phung N.T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
